deeb8046fd0b6b658cb1595a75dbe058763fb146
John Wallace <John.Wallace@noaa.gov>
2021-06-30 13:48:31 -0700

    Updated the bubble colors

--- R/plot.bubble.zero.cross.R
+++ R/plot.bubble.zero.cross.R
@@ -1,7 +1,8 @@
 plot.bubble.zero.cross <- function (xyzRaw, group = rep("A", nrow(xyzRaw)), Groups = unique(group), Circle.Area.Prop.to.Z = TRUE, maxsize = scale.size * diff(range(xyzRaw[, 2], na.rm = TRUE)), 
     scale.size = 0.07, largestSqrtZ = max(xyzSqrt[, 3], na.rm = TRUE), center.points = FALSE, center.cex = 0.5, xDelta = 0, add = FALSE, 
     xlab = dimnames(xyz)[[2]][1], ylab = dimnames(xyz)[[2]][2], main = NULL, range.bump = FALSE, cross.cex = 1, adj = NULL, 
-    fill.col = c("green", "red", "blue", "cyan", "black"), fill.col.alpha = 0.2,  border.col = "black", border.col.alpha = fill.col.alpha, 
+    fill.col = colorRampPalette(colors = c("dodgerblue", "orange", "cyan", "magenta", "green", "red", "blue", "violetred4", "cyan3", "mediumorchid3", "seagreen4", "black"))(N), fill.col.alpha = 0.2, 
+    border.col = "black", border.col.alpha = fill.col.alpha, 
     cross.col = {
         if (is.null(fill.col)) {
             border.col
@@ -11,28 +12,30 @@ plot.bubble.zero.cross <- function (xyzRaw, group = rep("A", nrow(xyzRaw)), Grou
         }
     }, cross.col.alpha = ifelse(fill.col.alpha + 0.65 > 1, 1, fill.col.alpha + 0.5), border.lwd = 1.25, PCH = FALSE, legend = TRUE, 
     legLoc = c(0.1, 0.25), legCol = "grey4", legAlpha = 0.5, legUnits = "Metric Tons", legNsmall = 1, Extra.Group.Size = rep(1, N), verbose = FALSE, ...)  {
+    
+    '  # **** Data is proportional to the area of the circle ****  '
 
-    " # Need to define below in case toolbox is not attached. "
+    '  # Need to define the fucntions below in case the toolbox is not attached. '
     '%>>%' <- function (x, y) {
         x > y & !is.na(x)
     }
 	
-
-    " **** Data is proportional to the area of the circle **** "
+    '  # Remainder function that gives back the divisor, not zero, when evenly divisible, e.g. 14 %r1% 7 gives 7 not 0  '
     "%r1%" <- function(e1, e2) ifelse(e1%%e2 == 0, e2, e1%%e2)
+    
+    '  # col.alpha() conveniently puts together the color and alpha level into a new color that can be directly used by functions like lines() and polygon(). '
     col.alpha <- function(col, alpha = 0.5) {
         FUNC <- function(col, alpha = alpha) {
             COL <- col2rgb(col)/255
             rgb(red = COL[1], green = COL[2], blue = COL[3], 
                 alpha = alpha)
         }
-        for (i in 1:length(col)) col[i] <- FUNC(col[i], alpha = alpha[i %r1% 
-            length(alpha)])
+        for (i in 1:length(col)) 
+             col[i] <- FUNC(col[i], alpha = alpha[i %r1% length(alpha)])
         col
     }
     
     
-
     if(nrow(xyzRaw) != length(group)) 
        stop("The number of rows of 'xyzRaw' needs to equal the length of the 'group' argument.")
     
@@ -56,6 +59,7 @@ plot.bubble.zero.cross <- function (xyzRaw, group = rep("A", nrow(xyzRaw)), Grou
     xyz <- xyz[tf, ]
     group <- group[tf]
     N <- length(Groups)
+   
 	if (!add) {
         if (range.bump) {
             xlim <- c(min(xyz[, 1], na.rm = T) - 0.2 * diff(range(xyz[, 
@@ -100,7 +104,7 @@ plot.bubble.zero.cross <- function (xyzRaw, group = rep("A", nrow(xyzRaw)), Grou
                    points(XYZ[i, 1], XYZ[i, 2], pch = 3, cex = cross.cex, col = cross.col[j %r1% length(cross.col)], lwd = 1)
                
                if (XYZ[i, 3] %>>% 0)
-                    JRWToolBox::circle.f(XYZ[i, 1], XYZ[i, 2], XYZ[i, 3] * Extra.Group.Size[j], adj = adj, fill.col = fill.col.a[j %r1% length(fill.col.a)], 
+                   JRWToolBox::circle.f(XYZ[i, 1], XYZ[i, 2], XYZ[i, 3] * Extra.Group.Size[j], adj = adj, fill.col = fill.col.a[j %r1% length(fill.col.a)], 
                         lwd = border.lwd[j %r1% length(border.lwd)], border.col = border.col[j %r1% length(border.col)], ...)
                        
                if(center.points)    
@@ -124,7 +128,7 @@ plot.bubble.zero.cross <- function (xyzRaw, group = rep("A", nrow(xyzRaw)), Grou
         }
         text(Usr[1] + (legLoc[1] + 0.05) * (Usr[2] - Usr[1]), Usr[3] + legLoc[2] * (Usr[4] - Usr[3]), legUnits, cex = ifelse(nchar(legUnits) < 20, 0.9, 0.7))
         if(cross.cex > 0) {
-            text(Usr[1] + legLoc[1] * (Usr[2] - Usr[1]), Usr[3] + (legLoc[2] - 0.03) * (Usr[4] - Usr[3]), "+")
+            text(Usr[1] + legLoc[1] * (Usr[2] - Usr[1]), Usr[3] + (legLoc[2] - 0.03) * (Usr[4] - Usr[3]), "+", col = cross.col, cex = cross.cex * 3)
             text(Usr[1] + (legLoc[1] + 0.125) * (Usr[2] - Usr[1]), Usr[3] + (legLoc[2] - 0.03) * (Usr[4] - Usr[3]), format(0, nsmall = legNsmall), adj = 1, cex = 0.9)
         }    
        #   circle.f(Usr[1] + legLoc[1] * (Usr[2] - Usr[1]), Usr[3] + (legLoc[2] - 0.075) * (Usr[4] - Usr[3]), maxsize * sqrt(Small)/max(xyzSqrt[, 3], na.rm = T), fill.col = col.alpha(legCol, legAlpha))
@@ -146,6 +150,7 @@ plot.bubble.zero.cross <- function (xyzRaw, group = rep("A", nrow(xyzRaw)), Grou
     }
     invisible()
 }
+
 
 
 
